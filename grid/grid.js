ed8ade9838070a1ffccf17aa437d210f8564d025
koome <kelvinkoomeh@gmail.com>
2024-04-29 22:38:42 +0300

    added a simple animation to the grid layout

---
+++ grid/grid.js
@@ -0,0 +1,38 @@
+function aboutMe() {
+    const mySkills = ["frontend development", "Backend developement", "DevOps Engineering", "Site Reliability Engineering"];
+    const aboutMeElement = document.querySelector(".about-me");
+    const writingSpeed = 100;
+    const erasingSpeed = 50;
+    const pause = 50;
+
+    let isDeleting = false;
+    let charIndex = 0;
+    let textIndex = 0;
+
+    function type() {
+        let currentText = mySkills[textIndex];
+        if (!isDeleting && charIndex < currentText.length) {
+            aboutMeElement.textContent += currentText[charIndex];
+            charIndex++;
+            setTimeout(type, writingSpeed);
+        }
+        else if (isDeleting && charIndex > 0) {
+            aboutMeElement.textContent = currentText.substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(type, erasingSpeed);
+        } else {
+            isDeleting = !isDeleting;
+            if (!isDeleting) {
+                textIndex = (textIndex + 1) % mySkills.length;
+
+
+            }
+            setTimeout(type, pause);
+        }
+
+    }
+
+    type();
+
+}
+aboutMe();
