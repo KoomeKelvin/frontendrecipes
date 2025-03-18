df05da1e8485187c63690e318bd0be0249f65b75
Koome Kelvin <your-email@example.com>
2025-03-18 14:22:09 +0300

    added changes to navbar

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const humbergerButton = document.querySelector('.humberger-button');
     humbergerButton.addEventListener('click', () => {
-        document.querySelector('.small-screen-menu').classList.toggle('display-small-screen-menu');
-        document.querySelector('.bars').classList.toggle('bars-is-closed');
-        document.querySelector('.cross').classList.toggle('cross-is-open');
+        document.querySelector('.hide-small-screen-menu').classList.toggle('display-small-screen-menu');
+        document.querySelector('.bars-button').classList.toggle('bars-button-is-closed');
+        document.querySelector('.cross-button').classList.toggle('cross-button-is-open');
     });
 });
